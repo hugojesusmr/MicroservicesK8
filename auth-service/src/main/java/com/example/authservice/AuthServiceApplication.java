ea1fbf2c74a2de2fadf4174a409f178ed646c278
hugojesusmr <hugojesusmr@gmail.com>
2022-08-16 16:41:43 +0000

    c

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -10,8 +10,6 @@ public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
-		int i = 0;
-          System.out.println("Valor"+i++);
 	}
 
 }
